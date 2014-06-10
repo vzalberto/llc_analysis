1c2296cc89031602e72102bb782830bc6dc6b096
vzalberto <vzalberto@msn.com>
2014-06-10 11:44:02 +0000

    gets first packet length

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>		//malloc()
 
-#define FILENAME "CARAMBA"
-#define BUFFER_SIZE 7
-
+#define FILENAME "llc_file"
+#define BUFFER_SIZE 4160
 
 unsigned short getPacketLength(unsigned char *MAC_portion)
 {
 	unsigned short length;
-	length = (MAC_portion[12] << 16) + MAC_portion[13];
+	
+	//sscanf()
+
 	return length;
 } 
 
 void printMAC_portion(unsigned char * MAC_portion)
 {
 	int i = 0;
-	while(i < 7)
+	while(i < 42)
 	{
-		printf("%02X ", MAC_portion[i]);
+		printf("%c", MAC_portion[i]);
 		i++;
 	}
 	printf("\n");
@@ -33,6 +35,37 @@ void file_into_buffer(unsigned char * buffer)
 	close(fp);
 }
 
+
+long firstPacketLength(unsigned char * buffer)
+{
+	unsigned char * packet;
+	unsigned long length;
+
+	unsigned long tmplong1, tmplong2;
+
+	unsigned char *a = (unsigned char *) malloc(3);
+	unsigned char *b = (unsigned char *) malloc(3);
+
+	a[0] = buffer[36];
+	a[1] = buffer[37];
+	a[2] = 0x00;
+
+	b[0] = buffer[39];
+	b[1] = buffer[40];
+	b[2] = 0x00;
+
+	tmplong1 = strtol(a, NULL, 16);
+	tmplong2 = strtol(b, NULL, 16);
+
+	length = (tmplong1 << 16) + tmplong2;
+
+	//free(a);
+	//free(b);
+
+	return length;
+}
+
+
 int main()
 {
 	unsigned char buffer[BUFFER_SIZE];
@@ -42,6 +75,8 @@ int main()
 	file_into_buffer(buffer);
 
 	printMAC_portion(buffer);
+
+	long len = firstPacketLength(buffer);
 
 	return 0;
 }
